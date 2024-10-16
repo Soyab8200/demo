2bac4d85465fd7848ff839bbb8649b432ccef074
Shoaib <soabghanchi9627@gmail.com>
2024-10-16 19:56:28 +0530

    last

---
+++ demo2/src/main/java/com/example/demo2/model/user.java
@@ -0,0 +1,9 @@
+package com.example.demo2.model;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.stereotype.Service;
+
+@EntityScan
+public class user {
+
+}
